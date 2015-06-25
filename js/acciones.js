f39625de806b4b21ff7f3874075cd9540661ffbf
Lanix 1 <Lanix 1>
2015-06-25 07:58:09 -0500

    uno
    equisde equisde equisde

---
+++ js/acciones.js
@@ -0,0 +1,84 @@
+// JavaScript Document
+$(document).ready(function(e) {
+//document.addEventListener("deviceready",function(){
+	
+	$('#conocimiento').on('click',function(){
+ $('audio')[0].play();
+ 
+	});
+	
+	$ ('#conocimiento') .on ('tap' , function () { 
+  navigator .vibrate (1000);
+ });//  tap a btnvibrar
+	
+
+	
+	$('#cientifico').on('click',function(){
+ $('audio')[0].play();
+ 
+	});
+	$ ('#cientifico') .on ('tap' , function () { 
+  navigator .vibrate (1000);
+ });//  tap a btnvibrar
+ 
+ 
+ 
+
+	$('#relacion').on('click',function(){
+ $('audio')[0].play();
+ 
+	});
+	$ ('#relacion') .on ('tap' , function () { 
+  navigator .vibrate (1000);
+ });//  tap a btnvibrar
+
+
+
+
+
+
+	$('#viajero').on('click',function(){
+ $('audio')[0].play();
+ 
+	});
+		$ ('#viajero') .on ('tap' , function () { 
+  navigator .vibrate (1000);
+ });//  tap a btnvibrar
+	
+	
+	
+	
+	$('#consecuencia').on('click',function(){
+ $('audio')[0].play();
+ 
+	});
+		$ ('#consecuencia') .on ('tap' , function () { 
+  navigator .vibrate (1000);
+ });//  tap a btnvibrar
+	
+	
+	
+	
+	$('#duda').on('click',function(){
+ $('audio')[0].play();
+ 
+	});
+		$ ('#duda') .on ('tap' , function () { 
+  navigator .vibrate (1000);
+ });//  tap a btnvibrar
+ 
+ 
+ $('#panel1').on('click',function(){
+ $('audio')[0].play();
+ 
+	});
+		$ ('#panel1') .on ('tap' , function () { 
+  navigator .vibrate (1000);
+ });//  tap a btnvibrar
+ 
+ $('#start').on('click',function(){
+ $('#Musica')[0].play();
+ 
+	});
+
+});
